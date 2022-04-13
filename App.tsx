4fc62eb483880a5d94aa92d01f91da24411c612f
Bombeta <paullo_jf@hotmail.com>
2022-04-13 19:57:33 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Signin } from './src/screens/Signin/';
+
+export default function App(){
+  return (
+    <Signin />
+  );
+}
